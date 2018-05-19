fbe27630f255b34ecb85b2733b08a1aa35105878
Nathan Hill <nateohill@gmail.com>
2018-05-19 01:06:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(array) {
-  while() {
-    
+  var i = 0;
+  while(i < array.length) {
+    array[i] = `${array[i]}!!!`
   }
+  return array;
 }
