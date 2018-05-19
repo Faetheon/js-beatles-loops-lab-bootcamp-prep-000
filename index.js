a608f7ebf187a0ca190aaf78d6a9210ad73da068
Nathan Hill <nateohill@gmail.com>
2018-05-19 00:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var mine = [];
+  for(i = 0; i < musicians.length; i++) {
+    mine.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return mine;
 }
